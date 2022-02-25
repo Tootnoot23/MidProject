75d1ee96eed0597240411de6358af2b90333467b
Tootnoot23 <tora.winarto@binus.ac.id>
2022-02-25 21:10:05 +0700

    upload

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+
+class BookController extends Controller
+{
+
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function create(Request $request)
+    {
+        // Book::create([
+        //     'Title' => $request->Title_book,
+        //     'Author' => $request->Author_book,
+        //     'Publisher' => $request->Publisher_book,
+        //     'Year' => $request->Year_book,
+        // ]);
+        $validated = $request->validate([
+            'Title' => ['required', 'string', 'between:5, 20'],
+            'Author' => ['required', 'string', 'between:5, 20'],
+            'Page' => ['required', 'integer', 'min:1'],
+            'Year' => ['required', 'integer', 'between:2000, 2021'],
+        ]);
+
+        if ($validated) {
+            Book::create([
+                'Title' => $request->Title,
+                'Author' => $request->Author,
+                'Page' => $request->Page,
+                'Year' => $request->Year,
+            ]);
+        }
+        return back();
+    }
+
+    public function show(Book $book)
+    {
+        $books = Book::all();
+        return view('list', compact('books'));
+    }
+
+    public function edit($id, Request $request)
+    {
+        $book = Book::findOrFail($id);
+        return view('update', compact('book'));
+    }
+
+    public function update($id, Request $request, Book $book)
+    {
+        Book::findOrFail($id)->update([
+            'Title' => $request->Title_book,
+            'Author' => $request->Author_book,
+            'Page' => $request->Page_book,
+            'Year' => $request->Year_book,
+        ]);
+        $books = Book::all();
+        return view('list', compact('books'));
+    }
+
+    public function destroy($id)
+    {
+        Book::destroy($id);
+        return back();
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.master')
+@section('title','index')
+@section('content')
+
+@if ($errors -> any())
+    <ul>
+        @foreach ($errors -> all() as $error)
+            <li>
+                {{$error}}
+            </li>
+        @endforeach
+    </ul>
+@endif
+<form action="{{route('create')}}" method="POST">
+    @csrf
+    <div class="mb-3">
+      <label for="Title_book" class="form-label">Title</label>
+      <input type="text" class="form-control" name="Title">
+    </div>
+    <div class="mb-3">
+        <label for="Author_book" class="form-label">Author</label>
+        <input type="text" class="form-control" name="Author">
+      </div>
+      <div class="mb-3">
+        <label for="Page_book" class="form-label">Page</label>
+        <input type="text" class="form-control" name="Page">
+      </div>
+      <div class="mb-3">
+        <label for="Year_book" class="form-label">Year</label>
+        <input type="text" class="form-control" name="Year">
+      </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+@endsection
